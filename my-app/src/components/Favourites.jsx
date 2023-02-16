b773b0a6767140fa5c537bc48cc092d275a299b3
Giacomo <giax.95@live.it>
2023-02-16 15:30:35 +0100

    Consegna momentantea.

---
+++ my-app/src/components/Favourites.jsx
@@ -0,0 +1,2 @@
+// Questo elenco di preferiti deve essere salvato interamente nel Redux Store, e renderizzato sotto forma di lista in una pagina separata (es. su una rotta /favourites).
+// Permetti all’utente anche di rimuovere un’azienda dalla lista dei preferiti.
